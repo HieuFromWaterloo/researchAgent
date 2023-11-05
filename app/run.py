33bbdf25248104e47a1554389b75a2c810be8528
HieuFromWaterloo <ng.hieu100@gmail.com>
2023-11-05 17:00:18 -0500

    updated current and added more prompts

--- app/run.py
+++ app/run.py
@@ -18,6 +18,7 @@ from flask import Flask, request, jsonify, make_response
 from flask_cors import CORS
 import logging
 from constants import *
+from llm_prompts import *
 import time
 from datetime import datetime
 import pytz
@@ -54,7 +55,6 @@ def search(query):
 def scrape_website(objective: str, url: str):
     # scrape website, and also will summarize the content based on objective if the content is too large
     # objective is the original objective & task that user give to the agent, url is the url of the website to be scraped
-
     print("Scraping website...")
     # Define the headers for the request
     headers = {
@@ -122,7 +122,7 @@ class ScrapeWebsiteInput(BaseModel):
 
 class ScrapeWebsiteTool(BaseTool):
     name = "scrape_website"
-    description = "useful when you need to get data from a website url, passing both url and objective to the function; DO NOT make up any url, the url should only be from the search results"
+    description = "This function is designed to extract data from a website URL based on a specified objective. Please provide both the URL and the objective as inputs. Ensure that the URL is sourced only from the search results and not fabricated. DO NOT make up any url"
     args_schema: Type[BaseModel] = ScrapeWebsiteInput
 
     def _run(self, objective: str, url: str):
@@ -137,23 +137,13 @@ tools = [
     Tool(
         name="Search",
         func=search,
-        description="useful for when you need to answer questions about current events, data. You should ask targeted questions"
+        description="This tool is valuable for addressing inquiries regarding current events and data. It's essential to formulate precise and targeted questions when using it."
     ),
     ScrapeWebsiteTool(),
 ]
 
 system_message = SystemMessage(
-    content="""You are a world class researcher, who can do detailed research on any topic and produce facts based results; 
-            you do not make things up, you will try as hard as possible to gather facts & data to back up the research
-            
-            Please make sure you complete the objective above with the following rules:
-            1/ You should do enough research to gather as much information as possible about the objective
-            2/ If there are url of relevant links & articles, you will scrape it to gather more information
-            3/ After scraping & search, you should think "is there any new things i should search & scraping based on the data I collected to increase research quality?" If answer is yes, continue; But don't do this more than 3 iterations
-            4/ You should not make things up, you should only write facts & data that you have gathered
-            5/ In the final output, You should include all reference data & links to back up your research; You should include all reference data & links to back up your research
-            6/ In the final output, You should include all reference data & links to back up your research; You should include all reference data & links to back up your research"""
-)
+    content=research_prompt1)
 
 agent_kwargs = {
     "extra_prompt_messages": [MessagesPlaceholder(variable_name="memory")],
